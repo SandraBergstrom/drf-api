b52740e25bcc03b3a1f328b65c748d04bf1b5d59
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-02 08:38:32 +0000

    Add postlistview

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,8 +3,8 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    owner = serlizers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerialiserMethodField()
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
