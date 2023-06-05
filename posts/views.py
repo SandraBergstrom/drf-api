01bd9cf2e242dcf899dae4a9f7f23177707dc13f
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-05 17:49:19 +0000

    Install auth

--- posts/views.py
+++ posts/views.py
@@ -11,13 +11,12 @@ class PostList(generics.ListCreateAPIView):
     List posts or create a post if logged in
     The perform_create method associates the post with the logged in user.
     """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
@@ -35,7 +34,7 @@ class PostList(generics.ListCreateAPIView):
     ordering_fields = [
         'likes_count',
         'comments_count',
-        'likes_created_at'
+        'likes__created_at',
     ]
 
     def perform_create(self, serializer):
@@ -48,7 +47,6 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
