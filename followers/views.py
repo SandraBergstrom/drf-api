8892d5145cd0f96cce195a86d2b0ad094a198c45
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-13 09:25:38 +0000

    test

--- followers/views.py
+++ followers/views.py
@@ -12,6 +12,12 @@ class FollowerList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+    try:
+        return super().create(request, *args, **kwargs)
+    except ValidationError as error:
+        print("Validation error:", error.detail)
+
 class FollowerDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = FollowerSerializer
